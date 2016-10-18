73dcd6959b22a20de8feaa42446e3bd15b760a72
john <john>
2016-10-18 15:53:18 +0000

    *** empty log message ***

---
+++ creatormain.tcl
@@ -0,0 +1,69 @@
+#!/usr/local/bin/tclsh
+
+package provide de1_creatormain 1.0
+
+package require de1_vars 
+package require de1_gui 
+package require de1_binary
+package require de1_utils 
+package require de1_machine
+
+##############################
+
+
+
+
+proc setup_images_for_other_pages {} {
+	#puts "setup_images_for_other_pages"
+
+	global screen_size_width
+	global screen_size_height
+
+	#global page_images
+
+
+	array set page_images [list \
+		"off" "[skin_directory]/nothing_on.png" \
+		"espresso" "[skin_directory]/espresso_on.png" \
+		"steam" "[skin_directory]/steam_on.png" \
+		"water" "[skin_directory]/tea_on.png" \
+		"settings" "[skin_directory]/settings_on.png" \
+		"sleep" "[skin_directory]/sleep.jpg" \
+		"tankfilling" "[skin_directory]/filling_tank.jpg" \
+		"tankempty" "[skin_directory]/fill_tank.jpg" \
+		"saver" [random_saver_file] \
+	]
+
+	# load each of the PNGs that get displayed for each espresso machine achivity
+	foreach {name pngfilename} [array get page_images] {
+		image create photo $name -file $pngfilename
+		.can create image {0 0} -anchor nw -image $name  -tag $name -state hidden
+	}
+
+	# debug log, will be invisible in release mode
+	.can create text 10 10 -text "" -anchor nw -tag .t -fill #000000 -font Helv_4 -width 1000
+
+	# set up the rectangles that define the finger tap zones and the associated command for each 
+	source "[skin_directory]/skin.tcl"
+
+}
+
+
+proc de1_creator_ui_startup {} {
+	setup_environment
+	setup_images_for_first_page
+	setup_images_for_other_pages
+
+	after $::settings(timer_interval) update_onscreen_variables
+
+	check_if_should_start_screen_saver
+	if {$::android == 1} {
+		ble_connect_to_de1
+		
+	} else {
+		after 1 run_de1_app
+	}
+	vwait forever
+}
+
+
---
+++ skinscreator/skinresize.tcl
@@ -0,0 +1,69 @@
+#!/usr/local/bin/tclsh 
+
+# skin resizer for creator skins
+
+cd /d/admin/code/de1beta/skinscreator/default/2560x1600
+
+set fast 0
+
+proc fast_write_open {fn parms} {
+    set f [open $fn $parms]
+    fconfigure $f -blocking 0
+    fconfigure $f -buffersize 1000000
+    return $f
+}
+
+proc write_file {filename data} {
+    set fn [fast_write_open $filename w]
+    puts $fn $data 
+    close $fn
+    return 1
+}
+
+proc read_file {filename} {
+    set fn [open $filename]
+    set data [read $fn]
+    close $fn
+    return $data
+}
+
+proc regsubex {regex in replace} {
+	set escaped [string map {\[ \\[ \] \\] \$ \\$ \\ \\\\} $in]
+	regsub -all $regex $escaped $replace result
+	set result [subst $result]
+	return $result
+}
+
+set skinfiles [concat [glob "*.jpg"] [glob "*.png"]] 
+set dirs [list \
+    "1280x800" 2 2 \
+    "1920x1200" 1.333333 1.333333 \
+    "1920x1080" 1.333333 1.4814814815 \
+    "1280x720"  2 2.22222 \
+    "2560x1440" 1 1.11111 \
+]
+
+# convert all the skin PNG files
+foreach {dir xdivisor ydivisor} $dirs {
+    #puts -nonewline "Making $dir skin $xdivisor / $ydivisor"
+    puts -nonewline "Making $dir skin."
+    if {$fast == 0} {
+        foreach skinfile $skinfiles {
+            puts -nonewline "."
+            flush stdout
+            exec convert $skinfile -resize $dir!  ../$dir/$skinfile &
+        }
+    }
+
+    set newskin [read_file "skin.tcl"]
+    set newskin [regsubex {add_de1_text (".*?") ([0-9]+) ([0-9]+) } $newskin "add_de1_text \\1 \[expr \{\\2/$xdivisor\}\] \[expr \{\\3/$ydivisor\}\] "]
+    set newskin [regsubex {add_de1_button (".*?") (.*?) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)\n} $newskin "add_de1_button \\1 \\2 \[expr \{\\3/$xdivisor\}\] \[expr \{\\4/$ydivisor\}\] \[expr \{\\5/$xdivisor\}\] \[expr \{\\6/$ydivisor\}\]\n"]
+    set newskin [regsubex {add_de1_variable (".*?") ([0-9]+) ([0-9]+) } $newskin "add_de1_variable \\1 \[expr \{\\2/$xdivisor\}\] \[expr \{\\3/$ydivisor\}\] "]
+    
+    # this the maximum text width for labels
+    #puts "xdivisor: $xdivisor"
+    set newskin [regsubex {\-width ([0-9]+)} $newskin "-width \[expr \{\\1/$xdivisor\}\] "]
+    write_file "../$dir/skin.tcl" $newskin 
+    puts "";
+
+}
