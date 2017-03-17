124e9be4c267f2c919bfaacf51c3536d03dee53a
john <john>
2017-03-17 06:58:02 +0000

    *** empty log message ***

--- skinresize.tcl
+++ skinresize.tcl
@@ -2,7 +2,8 @@
 
 source pkgIndex.tcl
 package require de1_utils
-skin_convert /d/admin/code/de1beta/skins/default/2560x1600
-skin_convert /d/admin/code/de1beta/skinsplus/default/2560x1600
-skin_convert /d/admin/code/de1beta/skinscreator/default/2560x1600
+package require de1_vars
 
+foreach d [lsort -increasing [skin_directories]] {
+	skin_convert "[homedir]/skins/$d/2560x1600"
+}
