974d877b60a3628e4cc8f7913f78383c47bec32c
john <john>
2017-05-22 09:46:38 +0000

    *** empty log message ***

--- skinresize.tcl
+++ skinresize.tcl
@@ -3,6 +3,7 @@
 source pkgIndex.tcl
 package require de1_utils
 package require de1_vars
+package provide de1plus 1.0
 
 foreach d [lsort -increasing [skin_directories]] {
 	skin_convert "[homedir]/skins/$d/2560x1600"
