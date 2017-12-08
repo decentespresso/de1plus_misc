faf0ab6fb150611ed5213a34e6bbd6db469f1463
john <john>
2017-12-08 10:01:22 +0000

    *** empty log message ***

---
+++ rename_photoshop_render_pages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+convert ~/Desktop/skin0000.tif nothing_on.png & 
+convert ~/Desktop/skin0001.tif espresso_on.png &
+convert ~/Desktop/skin0002.tif steam_on.png &
+convert ~/Desktop/skin0003.tif tea_on.png &
+wait
+
+rm  ~/Desktop/skin0000.tif 
+rm  ~/Desktop/skin0001.tif 
+rm  ~/Desktop/skin0002.tif 
+rm  ~/Desktop/skin0003.tif 
+
+zopflipng -q --iterations=1 -y   nothing_on.png nothing_on.png &
+zopflipng -q --iterations=1 -y   espresso_on.png espresso_on.png &
+zopflipng -q --iterations=1 -y   steam_on.png steam_on.png &
+zopflipng -q --iterations=1 -y   tea_on.png tea_on.png &
+wait
+
+exit
