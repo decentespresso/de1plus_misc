fcb31126fcce6940e31ea8535eb5b543a669604d
john <john>
2017-12-12 12:10:39 +0000

    *** empty log message ***

--- rename_photoshop_render_pages.sh
+++ rename_photoshop_render_pages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-convert ~/Desktop/skin0000.tif nothing_on.png & 
-convert ~/Desktop/skin0001.tif espresso_on.png &
-convert ~/Desktop/skin0002.tif steam_on.png &
-convert ~/Desktop/skin0003.tif tea_on.png &
+convert -strip ~/Desktop/skin0000.tif nothing_on.png & 
+convert -strip ~/Desktop/skin0001.tif espresso_on.png &
+convert -strip ~/Desktop/skin0002.tif steam_on.png &
+convert -strip ~/Desktop/skin0003.tif tea_on.png &
 wait
 
 rm  ~/Desktop/skin0000.tif 
@@ -13,10 +13,10 @@ rm  ~/Desktop/skin0003.tif
 
 convert nothing_on.png -resize 640x400 icon.jpg &
 
-zopflipng -q --iterations=1 -y   nothing_on.png nothing_on.png &
-zopflipng -q --iterations=1 -y   espresso_on.png espresso_on.png &
-zopflipng -q --iterations=1 -y   steam_on.png steam_on.png &
-zopflipng -q --iterations=1 -y   tea_on.png tea_on.png &
+zopflipng --always_zopflify --lossy_transparent -q --iterations=1 -y   nothing_on.png nothing_on.png &
+zopflipng --always_zopflify --lossy_transparent -q --iterations=1 -y   espresso_on.png espresso_on.png &
+zopflipng --always_zopflify --lossy_transparent -q --iterations=1 -y   steam_on.png steam_on.png &
+zopflipng --always_zopflify --lossy_transparent -q --iterations=1 -y   tea_on.png tea_on.png &
 wait
 
 exit
