ef502e6ea523e8f0cdbb817c8e8569a0d36ee0d2
john <john>
2017-12-22 08:58:32 +0000

    *** empty log message ***

--- skinresize.tcl
+++ skinresize.tcl
@@ -5,6 +5,4 @@ package require de1_utils
 package require de1_vars
 package provide de1plus 1.0
 
-foreach d [lsort -increasing [skin_directories]] {
-	skin_convert "[homedir]/skins/$d/2560x1600"
-}
+skin_convert_all
