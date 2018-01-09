43ecbdba9fa4adde4ffde57b62535f7abb7945c6
john <john>
2018-01-09 10:28:08 +0000

    *** empty log message ***

--- unde1plus.sh
+++ unde1plus.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /Applications/undroidwish.app/Contents/MacOS/undroidwish de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name DE1+
+#/Applications/undroidwish.app/Contents/MacOS/undroidwish de1plus.tcl  -sdlheight 1600 -sdlwidth 2560 -name DE1+
