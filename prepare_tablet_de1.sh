8b3ba414ead6c04dc216ed29adb63532949ff528
john <john>
2018-01-12 11:21:11 +0000

    *** empty log message ***

--- prepare_tablet_de1.sh
+++ prepare_tablet_de1.sh
@@ -12,49 +12,50 @@
 ###############################
 # set screen timeout to 30 minutes, when not plugged in
 # note: maybe not needed any longer since we are replacing the global settings.db
+echo "Setting screen timeout"
 adb shell settings put system screen_off_timeout 1800000
 ###############################
 
 ###############################
 # and for good measure disable screen timeout while plugged in
 # note: maybe not needed any longer since we are replacing the global settings.db
+echo "Setting screen on when plugged in"
 adb shell settings put global stay_on_while_plugged_in 3
 ###############################
 
 ###############################
 # enable wifi
 # note: maybe not needed any longer since we are replacing the global settings.db
+echo "Setting wifi on"
 adb shell settings put global wifi_on 1
 ###############################
 
-
 ###############################
 # make the display bright
+echo "Setting screen brightness to max"
 adb shell 'echo 255 > /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness'
+###############################
+
+###############################
+# install androwish
+echo "Installing Androwish"
+adb install android/androwish.apk
 ###############################
 
 ###############################
 # copy our source files over
 #adb push /d/download/sync/de1plus /mnt/sdcard/de1plus
+echo "Copying DE1 software"
 adb push /d/download/sync/de1 /mnt/sdcard/de1
 ###############################
 
 ###############################
-# install androwish
-adb install androwish/androwish.apk
-###############################
-
-###############################
-# create our app icons
-# note: need to manually drag the icons to their desired place, and also manually remove
-# note #2 : this may not be needed any longer since we are wiping out the launcher.db with our own in the next line
-adb shell am start -n tk.tcl.wish/.AndroWishLauncher -a android.intent.action.ACTION_VIEW -e arg file:///sdcard/de1/create_de1_icon.tcl
-###############################
-
-###############################
-# replace the launcher3 database with our own, which moves the icons where we want them and removes all tne toolbar noise of all those google icons
-#adb pull /data/data/com.android.launcher3/databases/launcher.db
-adb push android/launcher.db.de1 /data/data/com.android.launcher3/databases/launcher.db
+# install wallpaper
+#adb pull /data/system/users/0/wallpaper
+#adb pull /data/system/users/0/wallpaper_info.xml
+echo "Setting wallpaper"
+adb push android/wallpaper_info.xml /data/system/users/0/wallpaper_info.xml
+adb push android/wallpaper /data/system/users/0/wallpaper
 ###############################
 
 ###############################
@@ -66,19 +67,20 @@ adb push android/launcher.db.de1 /data/data/com.android.launcher3/databases/laun
 # secure/lockscreen.disabled 1
 # secure/bluetooth_name "Decent Tablet"
 #adb pull /data/data/com.android.providers.settings/databases/settings.db android/settings.db
-adb push settings.db /data/data/com.android.providers.settings/databases/settings.db
+echo "Replacing system settings"
+adb push android/settings.db /data/data/com.android.providers.settings/databases/settings.db
 ###############################
 
 ###############################
-# install wallpaper
-#adb pull /data/system/users/0/wallpaper
-#adb pull /data/system/users/0/wallpaper_info.xml
-adb push android/wallpaper_info.xml /data/system/users/0/wallpaper_info.xml
-adb push android/wallpaper /data/system/users/0/wallpaper
+# replace the launcher3 database with our own, which moves the icons where we want them and removes all tne toolbar noise of all those google icons
+#adb pull /data/data/com.android.launcher3/databases/launcher.db android/launcher.db.de1
+echo "Replacing launcher settings"
+adb push android/launcher.db.de1 /data/data/com.android.launcher3/databases/launcher.db
 ###############################
 
 ###############################
 # disable screen swipe lock
+echo "Disabling screen swipe to lock"
 adb shell am force-stop com.android.settings 
 adb shell input keyevent KEYCODE_HOME
 sleep 1
@@ -91,7 +93,21 @@ sleep 1
 adb shell am force-stop com.android.settings 
 ###############################
 
+
+###############################
+# create our app icons
+# note: need to manually drag the icons to their desired place, and also manually remove
+# note #2 : this is not needed any longer since we are wiping out the launcher.db with our own in the next line. This script is only used
+# to create the icons the first time, and then we use the sqlite table to restore them to a new tablet
+#adb shell am start -n tk.tcl.wish/.AndroWishLauncher -a android.intent.action.ACTION_VIEW -e arg file:///sdcard/de1/create_de1_icon.tcl
+###############################
+
+echo "all done... rebooting tablet"
 adb reboot
+#adb shell am force-stop com.android.launcher3 
+
+exit
+
 
 
 
