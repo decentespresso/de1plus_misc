01960a51edb90c8c76fa982152041d865ed8c24f
john <john>
2019-08-17 10:06:14 +0000

    *** empty log message ***

--- desktop_app/linux/de1_linux32.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-undroidwish/undroidwish-linux32 src/de1.tcl -sdlwidth 1280 -sdlheight 800 -name "DE1"
--- desktop_app/linux/de1_linux64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-undroidwish/undroidwish-linux64 src/de1.tcl -sdlwidth 1280 -sdlheight 800 -name "DE1"
-
