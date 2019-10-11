57f8a2a2176e10f0e657a0b3d1c163d8741c5351
john <john>
2019-10-11 11:01:21 +0000

    no message

--- tabletcopy.sh
+++ tabletcopy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+./makede1.tcl
+
 # back up settings
 adb pull /mnt/sdcard/de1plus/settings.tdb /tmp/settings.tdb
 
 adb shell rm -rf /mnt/sdcard/de1plus
 adb push /d/download/sync/de1plus /mnt/sdcard/de1plus
+
+#adb push *.tcl /mnt/sdcard/de1plus/
+#adb pull /mnt/sdcard/de1plus/log.txt /tmp/log.txt
 
 #restore settings
 adb push /tmp/settings.tdb /mnt/sdcard/de1plus/settings.tdb 
