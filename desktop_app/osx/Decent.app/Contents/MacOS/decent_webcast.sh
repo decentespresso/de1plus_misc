d43d7176eee3547a76950d639479c41da777f49a
john <john>
2019-11-13 00:54:39 +0000

    *** empty log message ***

--- desktop_app/osx/Decent.app/Contents/MacOS/decent_webcast.sh
+++ desktop_app/osx/Decent.app/Contents/MacOS/decent_webcast.sh
@@ -4,6 +4,7 @@ echo "You need FFMPEG to make this work https://www.ffmpeg.org/download.html\n"
 
 (sleep 1; echo "Connect to the Decent App at http://localhost:8080") &
 export SDL_VIDEODRIVER=jsmpeg
-#export SDL_VIDEO_JSMPEG_OUTFILE=/tmp/decent.mpg
+export SDL_VIDEO_JSMPEG_OUTFILE=~/Desktop/decent.mpg
 #export SDL_VIDEO_JSMPEG_AUTH=$(echo -n user:pass | base64)
-undroidwish/undroidwish-raspberry src/de1plus.tcl -sdlwidth 1280 -sdlheight 800 -name "Decent"
+cd ..
+MacOS/wish Resources/de1plus/de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
--- desktop_app/osx/Decent.app/Contents/MacOS/unde1plus.sh
+++ desktop_app/osx/Decent.app/Contents/MacOS/unde1plus.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-(sleep 1; echo "Connect to the Decent App at http://localhost:8080") &
-export SDL_VIDEODRIVER=jsmpeg
-export SDL_VIDEO_JSMPEG_OUTFILE=~/Desktop/decent.mpg
+# you need FFMPEG to make this work https://www.ffmpeg.org/download.html
+
 cd ..
-MacOS/undroidwish Resources/de1plus/de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
+MacOS/wish Resources/de1plus/de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
