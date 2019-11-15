fc540a79984a169d641d8833ce46c0232467c2c5
john <john>
2019-11-15 18:11:10 +0000

    *** empty log message ***

--- desktop_app/osx/Decent.app/Contents/MacOS/decent_webcast.sh
+++ desktop_app/osx/Decent.app/Contents/MacOS/decent_webcast.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "You need FFMPEG to make this work https://www.ffmpeg.org/download.html\n"
+echo "You need FFMPEG to make this work https://www.ffmpeg.org/download.html"
 
 (sleep 1; echo "Connect to the Decent App at http://localhost:8080") &
 export SDL_VIDEODRIVER=jsmpeg
 export SDL_VIDEO_JSMPEG_OUTFILE=~/Desktop/decent.mpg
+export SDL_VIDEO_JSMPEG_PORT 8080
 #export SDL_VIDEO_JSMPEG_AUTH=$(echo -n user:pass | base64)
 cd ..
 MacOS/wish Resources/de1plus/de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
--- desktop_app/win32/decent_webcast.bat
+++ desktop_app/win32/decent_webcast.bat
@@ -1,4 +1,5 @@
 echo "Connect to the Decent App at http://localhost:8080"
 set SDL_VIDEODRIVER=jsmpeg
 set SDL_VIDEO_JSMPEG_OUTFILE=%HOMEDRIVE%%HOMEPATH%\Desktop\decent.mpg
+set SDL_VIDEO_JSMPEG_PORT 8080
 undroidwish\undroidwish-win32.exe src\de1plus.tcl -sdlwidth 1280 -sdlheight 800 -name Decent
