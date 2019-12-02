196ca6b9da5255284aad9a1eebef1741bf67f20a
john <john>
2019-12-02 16:00:06 +0000

    *** empty log message ***

--- desktop_app/linux/decent_webcast_linux64.sh
+++ desktop_app/linux/decent_webcast_linux64.sh
@@ -4,7 +4,7 @@ echo "You need FFMPEG to make this work https://www.ffmpeg.org/download.html"
 
 (sleep 1; echo "Connect to the Decent App at http://localhost:8080") &
 export SDL_VIDEODRIVER=jsmpeg
-export SDL_VIDEO_JSMPEG_PORT 8080
+export SDL_VIDEO_JSMPEG_PORT=8080
 #export SDL_VIDEO_JSMPEG_OUTFILE=/tmp/decent.mpg
 #export SDL_VIDEO_JSMPEG_AUTH=$(echo -n user:pass | base64)
 undroidwish/undroidwish-linux64 src/de1plus.tcl -sdlwidth 1280 -sdlheight 800 -name "Decent"
