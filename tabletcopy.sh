de6abb6797d4f28191b4ca7f34a7c2473c533e49
john <john>
2019-12-06 00:44:32 +0000

    *** empty log message ***

--- tabletcopy.sh
+++ tabletcopy.sh
@@ -7,6 +7,7 @@ adb pull /mnt/sdcard/de1plus/settings.tdb /tmp/settings.tdb
 
 adb shell rm -rf /mnt/sdcard/de1plus
 adb push /d/download/sync/de1plus /mnt/sdcard/de1plus
+adb pull /sdcard/Pictures ~/Desktop
 
 #adb push *.tcl /mnt/sdcard/de1plus/
 #adb pull /mnt/sdcard/de1plus/log.txt /tmp/log.txt
