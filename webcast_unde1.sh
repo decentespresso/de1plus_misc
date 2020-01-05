fb908b0f8dcb207bcb1a067bc1d59379f53afa7b
john <john>
2020-01-05 17:39:12 +0000

    *** empty log message ***

---
+++ webcast_unde1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export SDL_VIDEODRIVER=jsmpeg
+#export SDL_VIDEO_JSMPEG_OUTFILE=~/Desktop/decent.mpg
+/opt/tmp/undroidwish-x86_64-deb9 de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
+#desktop_app/linux/undroidwish/undroidwish-linux64 
+#/Applications/undroidwish.app/Contents/MacOS/undroidwish de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
+
+
+#~/Desktop/undroidwish.app/Contents/MacOS/undroidwish de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
