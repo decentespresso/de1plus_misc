bd399e1ea2f1dbbf66674ec5904dcbdf096063b6
john <john>
2020-02-06 08:41:35 +0000

    *** empty log message ***

---
+++ fasttabletcopy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./makede1.tcl
+
+# back up settings
+#adb pull /mnt/sdcard/de1plus/settings.tdb /tmp/settings.tdb
+
+#adb shell rm -rf /mnt/sdcard/de1plus
+#adb push /d/download/sync/de1plus /mnt/sdcard/de1plus
+#adb pull /sdcard/Pictures ~/Desktop
+#adb pull /sdcard/Screenshots ~/Desktop
+
+#adb push *.tcl /mnt/sdcard/de1plus/
+adb pull /mnt/sdcard/de1plus/log.txt /tmp/log.txt
+mv /tmp/log.txt log_$SECONDS.txt
+adb push skins/Insight/*.tcl /mnt/sdcard/de1plus/skins/Insight;adb push skins/default/*.tcl /mnt/sdcard/de1plus/skins/default; adb push *.tcl /mnt/sdcard/de1plus/
+#
+
+#restore settings
+adb push /tmp/settings.tdb /mnt/sdcard/de1plus/settings.tdb 
