663dc16820cdf6debd2d67a07811ea303574b63f
john <john>
2020-08-02 06:19:45 +0000

    *** empty log message ***

--- unde1plus.sh
+++ unde1plus.sh
@@ -2,7 +2,8 @@
 
 #export SDL_VIDEODRIVER=jsmpeg
 #export SDL_VIDEO_JSMPEG_OUTFILE=~/Desktop/decent.mpg
-/Applications/undroidwish.app/Contents/MacOS/undroidwish de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
+/Applications/undroidwish.app/Contents/MacOS/undroidwish de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent -sdlresizable
+#/Applications/undroidwish.app/Contents/MacOS/undroidwish de1plus.tcl  -sdlheight 1280 -sdlwidth 1900 -sdlrootheight 1280 -sdlrootwidth 1900 -name Decent -sdlresizable
 
 #/Applications/undroidwish.app/Contents/MacOS/undroidwish de1plus.tcl  -sdlheight 960 -sdlwidth 1280 -sdlrootheight 960 -sdlrootwidth 1280 -name Decent
 
