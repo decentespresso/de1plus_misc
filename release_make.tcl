571f64de5a6832c8484d4bf40f0b1808d436c04d
john <john>
2020-09-22 10:16:32 +0000

    *** empty log message ***

--- release_make.tcl
+++ release_make.tcl
@@ -1,6 +1,6 @@
-#!/usr/local/bin/bash
+#!/usr/local/bin/tclsh
 
-echo "Making release version of de1beta->de1plus"
+puts "Making release version of de1beta->de1plus"
 
 file delete -force /d/download/desktop/osx/decent_osx_release.zip
 file delete -force /d/download/desktop/win32/decent_win_release.zip
@@ -8,14 +8,19 @@ file delete -force /d/download/desktop/linux/decent_linux_release.zip
 file delete -force /d/download/desktop/source/decent_source_release.zip
 file delete -force /d/download/sync/de1plus
 
+puts "- Mac"
 file copy -force /d/download/desktop/osx/decent_osx.zip /d/download/desktop/osx/decent_osx_release.zip
+
+puts "- Win"
 file copy -force /d/download/desktop/win32/decent_win.zip  /d/download/desktop/win32/decent_win_release.zip
+
+puts "- Linux"
 file copy -force /d/download/desktop/linux/decent_linux.zip /d/download/desktop/linux/decent_linux_release.zip
+
+puts "- Source"
 file copy -force /d/download/desktop/source/decent_source.zip /d/download/desktop/source/decent_source_release.zip
 
-file copy /d/download/desktop/osx/decent_osx.zip /d/download/desktop/osx/decent_osx_release.zip 
-file copy /d/download/desktop/linux/decent_linux.zip /d/download/desktop/linux/decent_linux_release.zip
-file copy /d/download/desktop/win32/decent_win.zip /d/download/desktop/win32/decent_win_release.zip 
+puts "- Sync"
 file copy -force /d/download/sync/de1beta /d/download/sync/de1plus
 
-echo "done"
+puts "done"
