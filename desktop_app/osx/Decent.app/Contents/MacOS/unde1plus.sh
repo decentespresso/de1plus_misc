63a51683284980b20ee288e7cb7d8591b378a950
John Buckman <john@decentespresso.com>
2021-02-04 15:27:25 +0800

    sdlheight=801 hack for OSX Big Sur

--- desktop_app/osx/Decent.app/Contents/MacOS/unde1plus.sh
+++ desktop_app/osx/Decent.app/Contents/MacOS/unde1plus.sh
@@ -3,4 +3,4 @@
 # you need FFMPEG to make this work https://www.ffmpeg.org/download.html
 
 cd ..
-MacOS/wish Resources/de1plus/de1plus.tcl  -sdlheight 800 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
+MacOS/wish Resources/de1plus/de1plus.tcl  -sdlheight 801 -sdlwidth 1280 -sdlrootheight 800 -sdlrootwidth 1280 -name Decent
