2d2f216cb681a9e2020d16eb5a7d60a63d816a46
Mimoja <git@mimoja.de>
2021-05-07 22:10:42 +0200

    Have the nightly script execute in the correct folder
    Signed-off-by: Mimoja <git@mimoja.de>

--- nightly_build.sh
+++ nightly_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ../de1app
+cd /d/admin/code/de1app
+git checkout de1plus/version.tcl
 git fetch --recurse-submodules
 git checkout origin/main -B main --force
 # This will update this script as it lives in misc
@@ -8,6 +9,6 @@ git submodule update --init --recursive
 
 cd de1plus
 echo "Current directory is $PWD"
-bash ../misc/create_build_info.sh  1 >/d/nightly.log 2>&1
+../misc/create_build_info.sh  1 >/d/nightly.log 2>&1
 ../misc/makede1.tcl 1 >>/d/nightly.log 2>&1
 cat /d/nightly.log
