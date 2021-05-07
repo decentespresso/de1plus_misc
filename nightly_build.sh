7d8441a44f2bec08000261bd9ca7f127bbecf9d5
Mimoja <git@mimoja.de>
2021-05-07 19:30:20 +0200

    Make the nightly script create the build-info file for now :)
    Signed-off-by: Mimoja <git@mimoja.de>

--- nightly_build.sh
+++ nightly_build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd /d/admin/code/de1app
-git pull
-git pull --recurse-submodules
+cd ../de1app
+git fetch --recurse-submodules
+git checkout origin/main -B main --force
+# This will update this script as it lives in misc
 git submodule update --init --recursive
 
 cd de1plus
-../misc/makede1.tcl 1 >/d/nightly.log 2>&1
+../misc/create_build_info.sh  1 >/d/nightly.log 2>&1
+../misc/makede1.tcl 1 >>/d/nightly.log 2>&1
 cat /d/nightly.log
