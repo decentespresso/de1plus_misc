7825395558b61176430067f2881b2604d0199f5a
Mimoja <git@mimoja.de>
2023-01-03 13:12:28 +0100

    Dont checkout psd folder for nightly builds
    Signed-off-by: Mimoja <git@mimoja.de>

--- nightly_build.sh
+++ nightly_build.sh
@@ -9,8 +9,8 @@ git fetch --recurse-submodules 2>&1 | tee -a /d/nightly.log
 git fetch --tags -f 2>&1 | tee -a /d/nightly.log 
 git checkout origin/main -B main --force 2>&1 | tee -a /d/nightly.log
 # This will update this script as it lives in misc
-git submodule update --init --recursive  2>&1 | tee -a /d/nightly.log
-git -C misc clean -xdf 2>&1 | tee -a /d/nightly.log 
+git -c submodule."psd".update=none submodule update --init --recursive 2>&1 | tee -a /d/nightly.log
+git -C misc clean -xdf 2>&1 | tee -a /d/nightly.log
 
 cd de1plus
 ../misc/create_build_info.sh 1 2>&1 | tee -a /d/nightly.log 
